2d551a580904eb8e902924ada468c7654644b478
cchck <kevinhu0428@outlook.com>
2026-06-14 14:46:42 +0800

    Fix:修复音乐专业剧情bug

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for 留学重开模拟器 PWA
 // Bump CACHE_VER to force re-cache after content updates
-const CACHE_VER = 'sasr-4.0';
+const CACHE_VER = 'sasr-4.2';
 
 const CORE_ASSETS = [
   './',
